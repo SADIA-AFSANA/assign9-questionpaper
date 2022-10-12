bb3ec0b4a9db775b5f2a8e0c1faa4fc14dea70d7
SADIA AFSANA <afsanasadia2022@gmail.com>
2022-10-12 12:34:35 +0600

    radio input one btndone

--- src/component/Option/Option.js
+++ src/component/Option/Option.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const Option = ({ option }) => {
-    const { options, id, correctAnswer } = option;
+    const { options, id, correctAnswer, } = option;
     return (
         <div>
-            <input type="radio" name='' id='' />
+            <input type="radio" name={`${id}`} id='anything' />{option}
         </div>
+
     );
+
 };
 
 export default Option;
