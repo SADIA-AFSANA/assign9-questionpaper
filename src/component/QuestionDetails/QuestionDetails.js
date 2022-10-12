efe78801d913a9e4a2af8fd0384a06dd8d828060
SADIA AFSANA <afsanasadia2022@gmail.com>
2022-10-12 14:24:51 +0600

    redmi file done

--- src/component/QuestionDetails/QuestionDetails.js
+++ src/component/QuestionDetails/QuestionDetails.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import Option from '../Option/Option';
 import './QuestionDetails.css'
+import { EyeIcon } from '@heroicons/react/24/solid'
 
 const QuestionDetails = ({ questionDetails }) => {
     const { id, name, questions, correctAnswer, options, question } = questionDetails;
     return (
-        <div className='quiz-component'>
+        <div>
+            <div className='quiz-component'>
 
+                <EyeIcon className="h-6 w-6 text-blue-500" />
+                <h4>{question}</h4>
+                {
+                    options.map(option => <Option
+                        key={option.id}
+                        option={option}
 
-            <h4>{question}</h4>
-            {
-                options.map(option => <Option
-                    key={option.id}
-                    option={option}
+                    ></Option>)
+                }
 
-                ></Option>)
-            }
-
-        </div>
+            </div></div>
     );
 };
 
